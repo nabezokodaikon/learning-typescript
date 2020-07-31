e49a598352938d23b931706f440a407fab51c38d
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-07-31 23:13:49 +0900

    Add List 1-39

--- chapter01/list01_37.ts
+++ chapter01/list01_37.ts
@@ -30,3 +30,26 @@ const songs = [
 const jukebox = new Jukebox(songs);
 
 jukebox.play();
+
+class Playlist {
+  private songs: Song[] = [];
+  static maxSongCount: number = 30;
+  
+  constructor(public name: string) {
+  }
+
+  addSong(song: Song) {
+    if (this.songs.length >= Playlist.maxSongCount) {
+      throw new Error('Playlist is full');
+    }
+    this.songs.push(song);
+  }
+}
+
+const playlist = new Playlist('My Playlist');
+
+const name = playlist.name;
+
+playlist.addSong(new Song('Therapy?', 'Hoge'));
+
+const MaxSongs = Playlist.maxSongCount;
