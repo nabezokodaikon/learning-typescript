878327ebac72afaca310d54f23ea72d19898a07f
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-03 22:04:32 +0900

    Add jQuery

---
+++ chapter02/list02_15.ts
@@ -0,0 +1,16 @@
+import * as $ from 'jquery';
+
+module list02_15 {
+
+  
+  
+
+  // declare class jQuery {
+    // html(html: string): void;
+  // }
+
+  // declare const $(query: string): jQuery;
+
+  $('#id').html('Hello World');
+  $('#id').html('2')
+}
