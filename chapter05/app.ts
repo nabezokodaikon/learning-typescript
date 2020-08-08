51f39ed6fae3b0797e9e55d79a7c295c331d66dd
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-09 00:58:00 +0900

    Add denon

---
+++ chapter05/app.ts
@@ -0,0 +1,10 @@
+/// <reference lib="dom" />
+
+const a = document.getElementById('content');
+console.log(a);
+
+const b = document.querySelector('#content');
+console.log(b);
+
+const c = document.querySelector('#content');
+console.log(c);
