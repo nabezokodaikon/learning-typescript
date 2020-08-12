a02e2720cf90ed35bb584703ec4ad0ac7423e39c
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-13 04:21:24 +0900

    Add List 6-2

--- chapter06/app.ts
+++ chapter06/app.ts
@@ -4,11 +4,12 @@ const server = serve({ port: 8000 });
 console.log('http://localhost:8000');
 
 for await (const req of server) {
-    req.respond({
-      status: 200,
-      headers: new Headers({
-        'Content-Type': 'text/plain',
-      }),
-      body: 'Response Text Here'
-    });
+  const body = `Mehotd: ${req.method}\nUrl: ${req.url}\nResponse Text Here`;
+  req.respond({
+    status: 200,
+    headers: new Headers({
+      'Content-Type': 'text/plain',
+    }),
+    body: body
+  });
 }
