80bef643d41411ab54da6a2c371ef3d709e31baf
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-13 06:10:34 +0900

    Add List 6-7

--- chapter06/app.ts
+++ chapter06/app.ts
@@ -3,12 +3,21 @@ import express from 'express';
 const server = express();
 const portNumber = 8080;
 
-const requestListener = (req: express.Request,
-                         res: express.Response) => {
-  res.send(`You requested ${req.query.firstname} ${req.query.lastname}`);
+// const requestListener = (req: express.Request,
+                         // res: express.Response) => {
+  // res.send(`You requested ${req.query.firstname} ${req.query.lastname}`);
+// };
+
+const handlerOne = (_req: express.Request, res: express.Response) => {
+  res.send('You got handlerOne');
 };
 
-server.get('/', requestListener);
+const handlerTwo = (_req: express.Request, res: express.Response) => {
+  res.send('You got handlerTwo');
+};
+
+server.get('/', handlerOne);
+server.get('/Test', handlerTwo);
 
 server.listen(portNumber, () => {
   console.log(`Listening on localhost: ${portNumber}`);
