0970458beedca82b32bb1d4d3924ec66ff69eb54
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-08-13 06:19:55 +0900

    Add List 6-7

--- chapter06/app.ts
+++ chapter06/app.ts
@@ -16,8 +16,18 @@ const handlerTwo = (_req: express.Request, res: express.Response) => {
   res.send('You got handlerTwo');
 };
 
+const handler = () => {
+  throw Error('Deliberate Error!');
+}
+
 server.get('/', handlerOne);
 server.get('/Test', handlerTwo);
+server.get('/Error', handler);
+
+server.use((error: any, _req: express.Request, res: express.Response, _next: any) => {
+  console.error(error.stack);
+  res.status(500).send('An error has occurred');
+});
 
 server.listen(portNumber, () => {
   console.log(`Listening on localhost: ${portNumber}`);
