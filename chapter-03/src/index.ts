81ce82f11cbbbb620a08de930675b5719bb7dcd1
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-12-12 02:41:33 +0900

    Add chapter-03 code

--- chapter-03/src/index.ts
+++ chapter-03/src/index.ts
@@ -4,3 +4,11 @@ function squareOf(n: number) {
 
 console.log(squareOf(2))
 // console.log(squareOf('z'))
+
+let a: unknown = 30
+console.log(a === 123)
+
+// console.log(a + 10)
+if (typeof a === 'number') {
+  console.log(a + 10)
+}
