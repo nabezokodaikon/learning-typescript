f766ce236d396266b2fc4dd07aee881377f084a3
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-12-12 22:57:17 +0900

    Add chapter-3.2.8

--- chapter-03/src/index.ts
+++ chapter-03/src/index.ts
@@ -1,14 +1,59 @@
-function squareOf(n: number) {
-  return n * n
+{
+  function squareOf(n: number) {
+    return n * n
+  }
+
+  console.log(squareOf(2))
+  // console.log(squareOf('z'))
+
+  let a: unknown = 30
+  console.log(a === 123)
+
+  // console.log(a + 10)
+  if (typeof a === 'number') {
+    console.log(a + 10)
+  }
+
+
+  const b = 1234n
+  console.log(b);
+
 }
 
-console.log(squareOf(2))
-// console.log(squareOf('z'))
+{
+  let c: {
+    firstName: string
+    lastName: string
+  } = {
+    firstName: 'john',
+    lastName: 'barrowman',
+  }
 
-let a: unknown = 30
-console.log(a === 123)
+  console.log(c)
 
-// console.log(a + 10)
-if (typeof a === 'number') {
-  console.log(a + 10)
+  class Person {
+    constructor(
+      public firstName: string,
+      public lastName: string,
+    ) {}
+  }
+
+  c = new Person('matt', 'smith')
+  console.log(c)
 }
+
+{
+  let a: {
+    b: number
+    c?: string
+    [key: number]: boolean
+  }
+
+  a = {b: 1}
+  a = {b: 1, c: undefined}
+  a = {b: 1, c: 'd'}
+  a = { b: 1, 10: true }
+  a = { b: 1, 10: true, 20: false }
+  console.log(a[10])
+}
+
