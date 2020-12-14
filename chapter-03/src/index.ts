e1cd9493dc4a9e36e8d4de988b7c7a0f17daadae
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-12-14 21:41:04 +0900

    Add chapter 3.2.9

--- chapter-03/src/index.ts
+++ chapter-03/src/index.ts
@@ -57,3 +57,86 @@
   console.log(a[10])
 }
 
+{
+  type Age = number
+
+  type Person = {
+    name: string
+    age: Age
+  }
+
+  let age = 55
+
+  let driver: Person = {
+    name: 'James May',
+    age: age
+  }
+
+  console.log(driver)
+
+  type Color = 'red'
+
+  let x = Math.random() < .5
+
+  if (x) {
+    type Color = 'blue'
+    let b: Color = 'blue'
+    console.log(b)
+  } else {
+    let c: Color = 'red'
+    console.log(c)
+  }
+}
+
+{
+  type Cat = { name: string, purrs: boolean }
+  type Dog = { name: string, barks: boolean, wags: boolean }
+  type CatOrDogOrBoth = Cat | Dog
+  type CatAndDog = Cat & Dog
+
+  // Cat
+  let a: CatOrDogOrBoth = {
+    name: 'Bonkers',
+    purrs: true,
+  }
+  console.log(a)
+
+  a = {
+    name: 'Domino',
+    barks: true,
+    wags: true,
+  }
+  console.log(a)
+
+  a = {
+    name: 'Donkers',
+    barks: true,
+    purrs: true,
+    wags: true,
+  }
+  console.log(a)
+
+  let b: CatAndDog = {
+    name: 'Domino',
+    barks: true,
+    purrs: true,
+    wags: true,
+  }
+  console.log(b);
+
+  type Returns = string | null
+  function trueOrNull(isTrue: boolean): Returns {
+    if (isTrue) {
+      return 'true'
+    } else {
+      return null
+    }
+  }
+  console.log(trueOrNull(true))
+
+  function hoge(a: string, b: number) {
+    return a || b
+  }
+  console.log(hoge('aaa', 1))
+  console.log(hoge('', 1))
+}
