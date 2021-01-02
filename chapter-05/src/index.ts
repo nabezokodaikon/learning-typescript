2dc87a72f01f09fe6c9d2887a63e1894ed04a91b
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-01-03 00:00:18 +0900

    Add chapter-5.7

--- chapter-05/src/index.ts
+++ chapter-05/src/index.ts
@@ -214,3 +214,19 @@ class Game {
     from(state: State): StringDatabase
   }
 }
+
+{
+  // class MyMap<K, V> {
+    // constructor(initialKey: K, initialValue: V) {
+    // }
+    // get(key: K): V {
+    // }
+    // set(key: K, value: V): void {
+
+    // }
+    // merge<K1, V1>(map: MyMap<K1, V1>): MyMap<K | K1, V1 | V1> {
+    // }
+    // static of<K, V>(k: K, v: V):MyMap<K, V> {
+    // }
+  // }
+}
