222c35efaa6de43aa51c252ad3ae6358ddf0f81a
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-01-06 23:45:50 +0900

    Completed chapter-6.2

--- chapter-06/src/index.ts
+++ chapter-06/src/index.ts
@@ -140,3 +140,29 @@
     }
   }
 }
+
+{
+  type Weekday = 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri'
+  type Day = Weekday | 'Sat' | 'Sun'
+
+  function getNextDay(w: Weekday): Day {
+    switch (w) {
+      case 'Mon': return 'Tue';
+      case 'Tue': return 'Tue';
+      case 'Wed': return 'Tue';
+      case 'Thu': return 'Tue';
+      case 'Fri': return 'Tue';
+    }
+  }
+
+  function isBig(n: number) {
+    if (n >= 100) {
+      return true
+    }
+  }
+
+  let nextDay = {
+    Mon: 'Tue'
+  };
+  nextDay.Mon;
+}
