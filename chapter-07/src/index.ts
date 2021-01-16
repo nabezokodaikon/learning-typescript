daff2878c1eaaddcd2e6e10313a224e47dadafe9
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-01-17 00:51:58 +0900

    Add chapter-7.2

--- chapter-07/src/index.ts
+++ chapter-07/src/index.ts
@@ -24,3 +24,41 @@
     // }
   // });
 }
+
+{
+  class InvalidDateFormatError extends RangeError {}
+  class DateIsInTHeFutureError extends RangeError {}
+
+  function isValid(date: Date) {
+    return Object.prototype.toString.call(date) === '[object Date]'
+      && !Number.isNaN(date.getTime());
+  }
+
+  /**
+   * @throws {InvalidDateFormatError}
+   * @throws {DateIsInTHeFutureError}
+   */
+  function parse(birthday: string): Date {
+    const date = new Date(birthday);
+    if (!isValid(date)) {
+      throw new InvalidDateFormatError('Enter a date in the from YYYY/MM/DD');
+    }
+    if (date.getTime() > Date.now()) {
+      throw new DateIsInTHeFutureError('Are you a timelord?');
+    }
+    return date
+  }
+
+  try {
+    const date = parse('a2021/01/18');
+    console.info('Date is ', date.toISOString());
+  } catch (e) {
+    if (e instanceof InvalidDateFormatError) {
+      console.error(e.message);
+    } else if (e instanceof DateIsInTHeFutureError) {
+      console.info(e.message);
+    } else {
+      throw e;
+    }
+  }
+}
