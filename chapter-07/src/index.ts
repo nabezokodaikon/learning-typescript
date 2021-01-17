2eb04a837ec2477358c9b5f983761eecdfa1fbfe
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-01-17 23:34:32 +0900

    Completed chapter-7

--- chapter-07/src/index.ts
+++ chapter-07/src/index.ts
@@ -49,18 +49,18 @@
     return date
   }
 
-  try {
-    const date = parse('a2021/01/18');
-    console.info('Date is ', date.toISOString());
-  } catch (e) {
-    if (e instanceof InvalidDateFormatError) {
-      console.error(e.message);
-    } else if (e instanceof DateIsInTHeFutureError) {
-      console.info(e.message);
-    } else {
-      throw e;
-    }
-  }
+  // try {
+    // const date = parse('a2021/01/18');
+    // console.info('Date is ', date.toISOString());
+  // } catch (e) {
+    // if (e instanceof InvalidDateFormatError) {
+      // console.error(e.message);
+    // } else if (e instanceof DateIsInTHeFutureError) {
+      // console.info(e.message);
+    // } else {
+      // throw e;
+    // }
+  // }
 }
 
 {
@@ -85,12 +85,111 @@
     return date;
   }
 
-  const result = parse('a2021/01/18');
-  if (result instanceof InvalidDateFormatError) {
-    console.error(result.message);
-  } else if (result instanceof DateIsInTheFutureError) {
-    console.error(result.message);
-  } else {
-    console.info('Date is', result.toISOString());
+  // const result = parse('a2021/01/18');
+  // if (result instanceof InvalidDateFormatError) {
+    // console.error(result.message);
+  // } else if (result instanceof DateIsInTheFutureError) {
+    // console.error(result.message);
+  // } else {
+    // console.info('Date is', result.toISOString());
+  // }
+}
+
+{
+  function isValid(date: Date) {
+    return Object.prototype.toString.call(date) === '[object Date]'
+      && !Number.isNaN(date.getTime());
   }
+
+  function parse(birthday: string): Date[] {
+    const date = new Date(birthday);
+    if (!isValid(date)) {
+      return [];
+    }
+    return [date];
+  }
+
+  function ask() {
+    const result = '2001/01/01';
+    if (result === null) {
+      return [];
+    }
+    return [result];
+  }
+
+  function flatten<T>(array: T[][]): T[] {
+    return Array.prototype.concat.apply([], array);
+  }
+
+  const result = flatten(ask()
+    .map(parse))
+    .map(date => date.toISOString());
+  // console.log(result);
+}
+
+{
+  interface Option<T> {
+    flatMap<U>(f: (value: T) => None): None
+    flatMap<U>(f: (value: T) => Option<U>): Option<U>
+    getOrElse(value: T): T
+  }
+  class Some<T> implements Option<T> {
+    constructor(private value: T) {}
+    flatMap<U>(f: (value: T) => None): None
+    flatMap<U>(f: (value: T) => Some<U>): Some<U>
+    flatMap<U>(f: (value: T) => Option<U>): Option<U> {
+      return f(this.value);
+    }
+    getOrElse(): T {
+      return this.value;
+    }
+  }
+  class None implements Option<never> {
+    flatMap(): None {
+      return this;
+    }
+    getOrElse<U>(value: U): U {
+      return value;
+    }
+  }
+
+  function Option<T>(value: null | undefined): None
+  function Option<T>(value: T): Some<T>
+  function Option<T>(value: T): Option<T> {
+    if (value === null) {
+      return new None;
+    }
+    return new Some(value);
+  }
+
+  const result = Option(6)
+    .flatMap(n => Option(n * 3))
+    .flatMap(n => new Some(n))
+    .getOrElse();
+  console.log(result);
+
+  function isValid(date: Date) {
+    return Object.prototype.toString.call(date) === '[object Date]'
+      && !Number.isNaN(date.getTime());
+  }
+
+  function parse(birthday: string): Option<Date> {
+    const date = new Date(birthday);
+    if (!isValid(date)) {
+      return new None();
+    }
+    return new Some(date);
+  }
+
+  function ask() {
+    const result = '2001/01/01';
+    if (result === null) {
+      return new None();
+    }
+    return new Some(result);
+  }
+
+  // ask()
+    // .flatMap(parse)
+    // .flatMap(date => new Some(date.toISO)) 
 }
