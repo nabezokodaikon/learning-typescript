673ad6670a6df8f1aa2df2178f76e02bb865d539
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-01-20 23:28:50 +0900

    Add chapter-8.2

--- chapter-08/src/index.ts
+++ chapter-08/src/index.ts
@@ -1,5 +1,32 @@
+import * as fs from 'fs';
+
 {
   setTimeout(() => console.info('A'), 1);
   setTimeout(() => console.info('B'), 2);
   console.info('C');
 }
+
+{
+  fs.readFile(
+    './src/sample.txt',
+    {encoding: 'utf8'},
+    (error, data) => {
+      if (error) {
+        console.error('error reading!', error);
+        return;
+      }
+      console.info('success reading!', data);
+    }
+  );
+
+  fs.appendFile(
+    './src/sample.txt',
+    'New access log entry',
+    error => {
+      if (error) {
+        console.error('error writing!', error);
+      }
+    }
+  );
+}
+
