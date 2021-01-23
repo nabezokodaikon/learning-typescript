5a7b5c7642a6abfbb777aa70e92ab7f3244eb07a
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-01-23 20:15:26 +0900

    Add chapter-8.3

--- chapter-08/src/index.ts
+++ chapter-08/src/index.ts
@@ -30,3 +30,36 @@ import * as fs from 'fs';
   );
 }
 
+{
+  // function appendAndReadPromise(path: string, data: string): Promise<string> {
+    // return appendPromise(path, data)
+      // .then(() => readPromise(path))
+      // .catch(error => console.error(error));
+  // }
+
+  // type Executor<T, E extends Error> = (
+    // resolve: (result: T) => void,
+    // reject: (error: E) => void,
+  // ) => void
+
+  // class Promise<T, E extends Error> {
+    // constructor(f: Executor<T, E>) {}
+    // then<U, F extends Error>(g: (result: T) => Promise<U, F> | U): Promise<U, F>
+    // catch<U, F extends Error>(g: (error: E) => Promise<U, F> | U): Promise<U, F>
+  // }
+
+  // readFile(path, (error, result) => {
+  // }); 
+
+  // function readFilePromise(path: string): Promise<string> {
+    // return new Promise((resolve, reject) => {
+      // readFile(path, (error, result) => {
+        // if (error) {
+          // reject(error);
+        // } else {
+          // resolve(result);
+        // }
+      // })
+    // })
+  // }
+}
